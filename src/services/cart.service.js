ad40e8c08e052f8e616c73dbe296b7bcb0942d01
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-12-28 00:40:23 -0300

    ya esta listo solo falla el boton no elimina los productos no se porque

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -97,11 +97,19 @@ const purchase = async (cid, user) => {
         }
       });
 
+      const formattedTicket = `
+      <h3>Tu ticket de compra</h3>
+      <p>Código: ${ticket.code}</p>
+      <p>Fecha de compra: ${ticket.purchase_datetime}</p>
+      <p>Monto: $${ticket.amount}</p>
+      <p>Comprador: ${ticket.purchaser}</p>
+  `;
+
       await transporter.sendMail({
         from: 'gabriellagrotteria18@gmail.com', // Reemplaza con el remitente deseado
-        to: user, // Dirección de correo del usuario
+        to: 'gabriellagrotteria18@gmail.com', // Dirección de correo del usuario
         subject: 'Ticket de compra',
-        html: `<h3>Tu ticket de compra</h3><br>${JSON.stringify(ticket, null, 2)}` // Puedes personalizar el formato del correo
+        html: formattedTicket // Puedes personalizar el formato del correo
       });
 
       console.log(ticket)
@@ -115,10 +123,6 @@ const purchase = async (cid, user) => {
       session.endSession();
     }
 
-      // Notificar usuario (si hay productos sin stock)
-  if (outStock.length > 0) {
-    console.log(`Los siguientes productos no pudieron ser comprados porque no hay stock suficiente:\n${outStock.map(({ product }) => product.name).join('\n')}`);
-  }
 
   // Vaciar carrito
   cart.products = [];
